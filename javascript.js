6642561d72eb848442ecf1af7088dc6a49129b9e
wilson alexander <wilgm0kdev@gmail.com>
2024-07-30 00:00:57 -0500

    botones

--- javascript.js
+++ javascript.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-    alert("hola desde jquery");
+    // alert("hola desde jquery");
+//    let varBotton = $('button').html();
+//    console.log(varBotton);
+    // $('button').html("cambiado desde jquery")
+    // $('.btn').html("cambiando desde la clase");
+    // $('#boton').html("cambiado desde el id");
+    // $('#subcontenedor button').html("boton dentro del subcontenedor");
+    $('button:first').html("boton primero");
+    $('button:last').html("ultimo");
+     
+
 });
+
+// document.querySelector('#boton').innerHTML= " cambiando desde js";
+
