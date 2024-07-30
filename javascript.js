414839761a5cfa412223c84b1045f931f1b364dc
wilson alexander <wilgm0kdev@gmail.com>
2024-07-30 00:06:31 -0500

    añadir y quitar clases

--- javascript.js
+++ javascript.js
@@ -1,16 +1,7 @@
 $(document).ready(function(){
-    // alert("hola desde jquery");
-//    let varBotton = $('button').html();
-//    console.log(varBotton);
-    // $('button').html("cambiado desde jquery")
-    // $('.btn').html("cambiando desde la clase");
-    // $('#boton').html("cambiado desde el id");
-    // $('#subcontenedor button').html("boton dentro del subcontenedor");
-    $('button:first').html("boton primero");
-    $('button:last').html("ultimo");
-     
+    $('#boton').addClass('btn-danger');
+    $('#boton').removeClass('btn-primary');
 
 });
 
-// document.querySelector('#boton').innerHTML= " cambiando desde js";
 
