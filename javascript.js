2bafdd7368124409f3f8237a8f1ffe2b375ef774
wilson alexander <wilgm0kdev@gmail.com>
2024-08-02 18:43:50 -0500

    como añadir css clases y html

--- javascript.js
+++ javascript.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
     $('#boton').addClass('btn-danger');
     $('#boton').removeClass('btn-primary');
 
+    $('#subcontenedor').append('<button type="button" class="btn btn-danger">agregado con append</button>');
+    $('#subcontenedor').prepend('<button type="button" class="btn btn-danger">agregado con prepend</button>');
+
+    // $('#boton').css('color', 'blue');
+    // $('#boton').css('background-color', 'yellow');
+
+    $('#boton').css({'color' : 'blue', 'background-color' : 'yellow'});
+    // $('#boton').css('background-color', 'yellow');
+    
 });
 
 
