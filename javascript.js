6b7979d9e27937c56bed58d10ad8f5be348454b9
wilson alexander <wilgm0kdev@gmail.com>
2024-08-02 20:49:32 -0500

    trabajando con animate

--- javascript.js
+++ javascript.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         let margen = $('img').css('margin-left');
         margen = parseInt( margen.replace('px', ''));
         margen = margen+30;
-        $('img').css('margin-left', margen+ 'px');
+        $('img').css('margin-left', margen + 'px');
         
     });
     $('#subcontenedor button:nth-child(3)').click(function (e) { 
@@ -61,6 +61,29 @@ $(document).ready(function(){
         $('img').slideToggle(300);
         
     });
+    $('#subcontenedor button:nth-child(12)').click(function (e) { 
+        e.preventDefault();
+        $('img').animate({'margin-left': '+=100px',
+            'opacity' : '0.5',
+            'width': '+=150px'
+        },2000,
+        function(){
+            $('img').animate({'margin-left': '-=100px',
+                'opacity' : '1',
+                'width': '200px'
+            }, 2000);  
+        }
+    );
+        
+    });
+    $('#subir').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0},500)
+    })
+    $('#bajar').click(function(e){
+        e.preventDefault();
+        $('html, body').animate({scrollTop: $(document).height()-$(window).height()},500) // baja el scroll
+    })
     let textoUI =$('#texto');
     textoUI.keyup(function (e) { 
         
@@ -75,4 +98,4 @@ $(document).ready(function(){
 
 });
 
-
+///https://www.youtube.com/watch?v=e-WpE3slYvU&list=PLCTD_CpMeEKSYJ1J15M8PknOMwOpeqsXz&index=8
