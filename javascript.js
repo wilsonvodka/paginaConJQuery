ae42550f8c5a3f5939efa9c5bae1e3523d5e99a2
wilson alexander <wilgm0kdev@gmail.com>
2024-08-02 20:08:01 -0500

    trabajando con hide show fadeIn fadeOut sldeDown slideUp

--- javascript.js
+++ javascript.js
@@ -21,7 +21,56 @@ $(document).ready(function(){
         $('img').toggleClass('d-none');
         
     });
-
+    $('#subcontenedor button:nth-child(4)').click(function (e) { 
+        e.preventDefault();
+        $('img').hide('slow');
+        
+    });
+    $('#subcontenedor button:nth-child(5)').click(function (e) { 
+        e.preventDefault();
+        $('img').show('slow');
+        
+    });
+    $('#subcontenedor button:nth-child(6)').click(function (e) { 
+        e.preventDefault();
+        $('img').toggle('fast');
+        
+    });
+    $('#subcontenedor button:nth-child(7)').click(function (e) { 
+        e.preventDefault();
+        $('img').fadeIn(300);
+        
+    });
+    $('#subcontenedor button:nth-child(8)').click(function (e) { 
+        e.preventDefault();
+        $('img').fadeOut(300);
+        
+    });
+    $('#subcontenedor button:nth-child(9)').click(function (e) { 
+        e.preventDefault();
+        $('img').slideDown(300);
+        
+    });
+    $('#subcontenedor button:nth-child(10)').click(function (e) { 
+        e.preventDefault();
+        $('img').slideUp(300);
+        
+    });
+    $('#subcontenedor button:nth-child(11)').click(function (e) { 
+        e.preventDefault();
+        $('img').slideToggle(300);
+        
+    });
+    let textoUI =$('#texto');
+    textoUI.keyup(function (e) { 
+        
+        $('#saludo').html("Hola "+ $('#texto').val());
+    });
+    $('form').submit(function (e) { 
+        alert('hola');
+        e.preventDefault();
+        
+    });
 
 
 });
