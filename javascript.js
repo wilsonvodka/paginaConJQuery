136bf1eb7838fef404a484ac877582e6dd8dff5d
wilson alexander <wilgm0kdev@gmail.com>
2024-08-03 19:57:09 -0500

    opciones del slider

--- javascript.js
+++ javascript.js
@@ -1,9 +1,22 @@
 $(document).ready(function(){
     // $(".slider").bxSlider();
-    $(".bxslider").bxSlider();
+    $(".bxslider").bxSlider({
+        auto: true,
+        speed:100, //velocidad del cambio de imagen
+        autoControls:true, //bototes de reproduccion
+        stopAutoOnClick: true, // se detiene la imagen al dar click en los botones con forma de circulos
+        pager: true,
+        pagerType: 'short', //opciones : full, short
+        // slideWidth:500, //cambia el tamaño del slider
+        mode:'horizontal', //opciones: vertical , horizontal
+        // randomStart: true,
+        infiniteLoop:true,// si esta en false se detiene al final
+        
+    });
+
 
 
 });
 
-
+// https://bxslider.com/
 
