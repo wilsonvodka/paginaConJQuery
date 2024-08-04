e41698f2c47005bd2e443d7531ae1314ab01af3a
wilson alexander <wilgm0kdev@gmail.com>
2024-08-03 19:35:56 -0500

    slider

--- javascript.js
+++ javascript.js
@@ -1,34 +1,9 @@
 $(document).ready(function(){
-    $('#fecha').daterangepicker({
-        locale:{
-            format:"YYYY-MM-DD hh:mm:ss"
-        },
-        timePicker: true,
-        startDate: moment().startOf('hour'),
-        endDate: moment().startOf('hour').add(48,'hour')
-
-    },function(inicio,fin){
-        console.log(inicio.format("YYYY-MM-DD hh:mm:ss") + " - "+fin.format("YYYY-MM-DD hh:mm:ss"));
-    });
-    $('#fecha2').daterangepicker({
-        locale:{
-            format:"YYYY-MM-DD hh:mm:ss"
-        },
-        singleDatePicker: true,
-        showDropdowns:true,
-        minYear:2000,
-        maxYear:moment().format('YYYY'),
-        timePicker: true,
-        startDate: moment().startOf('hour'),
-        endDate: moment().startOf('hour').add(48,'hour')
-
-    },function(inicio,fin){
-        console.log(inicio.format("YYYY-MM-DD hh:mm:ss") + " - "+fin.format("YYYY-MM-DD hh:mm:ss"));
-    });
+    // $(".slider").bxSlider();
+    $(".bxslider").bxSlider();
 
 
 });
 
-//https://www.daterangepicker.com/
 
 
