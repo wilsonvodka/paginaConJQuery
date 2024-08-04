07a19568945da3b08259c6270138ff5903999c61
wilson alexander <wilgm0kdev@gmail.com>
2024-08-03 20:27:22 -0500

    ajax

---
+++ javascript2.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('#leer').click(function (e) { 
+        e.preventDefault();
+        $.get("archivo.txt",function(data,textStatus,jqXHR){
+            console.log(data)
+            console.log(textStatus)
+            console.log(jqXHR)
+        })
+    });
+    $('#leerEmpleado').click(function (e) { 
+        e.preventDefault();
+        $.get("empleados.json", function(data){
+            console.log(data);
+            $("#datosEmpleado").html(`
+                Nombre: ${data.nombre} <br>
+                Puesto: ${data.puesto}<br>
+                Edad:${data.edad}<br>
+                `);
+        })
+    });
+});
